310c53418de8c38d9c8b271eebc5c76d38abe387
OnMyWayBackHome <onmywaybackhome@gmail.com>
2024-06-17 13:28:37 -0400

    feet and inches

--- ch2d_investment_value.py
+++ ch2d_investment_value.py
@@ -1,5 +1,15 @@
-# Interest calculator
-my_balance = float(input("What is your balance? "))
+def investment_growth(value, rate, years):
+    """
+    Calculates the value of an investment.
+    :param value: initial value of the investment in dollars.
+    :param rate: interest rate expressed as a percent
+    :param years: number of years that the investment grows
+    :return: new value rounded to the nearest cent.
+    """
+    return value * (1 + rate)**years
+
+
+balance = float(input("What is your balance? "))
 my_interest = float(input("What's your interest rate? "))
 years = float(input("How many years of growth? "))
 my_balance = my_balance * (1 + my_interest) ** years
