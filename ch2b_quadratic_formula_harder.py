7503e272ccb5447a2f7e63beaca54b81878888ff
OnMyWayBackHome <onmywaybackhome@gmail.com>
2024-06-19 11:54:45 -0400

    tweaks to align with codeHS

--- ch2b_quadratic_formula_harder.py
+++ ch2b_quadratic_formula_harder.py
@@ -3,19 +3,21 @@ import random
 
 def quadratic_formula(a, b, c):
     """
-    Finds the zeros of a quadratic function using the quadratic formula
-    :param a, b, and c: coefficients in standard form: ax**2 + bx + c. Assume that a is not zero.
-    :return: tuple with two solution, with the first value being the one computed by adding the square root of the determinant
+    Finds the real zeros of a quadratic function using the quadratic formula
+    :param a, b, and c: coefficients of quadratic in standard form: ax**2 + bx + c.
+    :return: tuple with two values of x or "None" if the result is complex or if a == 0.
     """
     determinant = b ** 2 - 4 * a * c
-    x1 = (-b + determinant ** 0.5) / (2 * a)
-    x2 = (-b - determinant ** 0.5) / (2 * a)
-    return x1, x2
+    if a == 0 or determinant < 0:
+        return None
+
+    return ((-b + determinant ** 0.5) / (2 * a),
+            (-b - determinant ** 0.5) / (2 * a))
 
 
 
 if __name__ == '__main__':
-    test_cases = [(-4, 1, 2), (1, -6, 9), (3, 1, 0), (5, 0, 1), (5, 0, -1), (4, 5, 6), (-4.9, 10, 12) ]
+    test_cases = [(0, 1, 2), (1, -6, 9), (3, 1, 0), (5, 0, 1), (5, 0, -1), (4, 5, 6), (-4.9, 10, 12) ]
     random_cases = [(random.random() * 100 - 50, random.random() * 100 - 50, random.random() * 100 - 50) for _ in range(5)]
     for a, b, c in test_cases + random_cases:
         if quadratic_formula(a, b, c) is not None:
